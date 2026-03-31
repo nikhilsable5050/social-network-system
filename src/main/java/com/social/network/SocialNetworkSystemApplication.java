0af6e5a2cd2c0871834b12fa69347e4d320a1f3b
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-03-31 18:38:08 +0530

    Initial Spring Boot social network project setup

---
+++ src/main/java/com/social/network/SocialNetworkSystemApplication.java
@@ -0,0 +1,13 @@
+package com.social.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social/network/SocialNetworkSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
